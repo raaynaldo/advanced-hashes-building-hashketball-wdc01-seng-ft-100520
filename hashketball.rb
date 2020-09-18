837379bb88721442e5a902e9cda3841a8ca29d5d
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 20:48:56 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,15 @@ def game_hash
           :slam_dunks=> 1
         },
         {
-
+          :player_name => "Reggie Evans",
+          :number => 30,
+          :shoe=> 14,
+          :points=> 12,
+          :rebounds=> 12,
+          :assists=> 12,
+          :steals=>12,
+          :blocks=> 12,
+          :slam_dunks=> 7
         }
 
       ]
