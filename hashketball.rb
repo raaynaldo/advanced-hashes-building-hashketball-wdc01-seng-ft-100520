81c6b5d1520e87a4ac55e701560c18a1f0ace67d
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 20:53:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -102,26 +102,26 @@ def game_hash
           :slam_dunks=> 5
         },
         {
-          :player_name => "Mason Plumlee",
-          :number => 1,
-          :shoe=> 19,
-          :points=> 26,
-          :rebounds=> 11,
-          :assists=> 6,
-          :steals=> 3,
-          :blocks=> 8,
-          :slam_dunks=> 5
+          :player_name => "Ben Gordon",
+          :number => 8,
+          :shoe=> 15,
+          :points=> 33,
+          :rebounds=> 3,
+          :assists=> 2,
+          :steals=> 1,
+          :blocks=> 1,
+          :slam_dunks=> 0
         },
         {
-          :player_name => "Jason Terry",
-          :number => 31,
+          :player_name => "Kemba Walker",
+          :number => 33,
           :shoe=> 15,
-          :points=> 19,
-          :rebounds=> 2,
-          :assists=> 2,
-          :steals=> 4,
-          :blocks=> 11,
-          :slam_dunks=> 1
+          :points=> 6,
+          :rebounds=> 12,
+          :assists=> 12,
+          :steals=> 7,
+          :blocks=> 5,
+          :slam_dunks=> 12
         },
       ]
     }
