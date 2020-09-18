1e5b3377d27b043b927d6792f63a4a73db969900
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 20:43:57 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,19 +3,19 @@
 def game_hash
   require "pry"
   return {}
-  # return {
-  #   :home=>{
-  #     :team_name => ""
-  #     :colors => ""
-  #     :players => {
-  #       :player_name => "",
-  #
-  #     }
-  #   },
-  #   :away=>{
-  #     :team_name => ""
-  #     :colors => ""
-  #     :players => ""
-  #   }
-  # }
+  return {
+    :home=>{
+      :team_name => ""
+      :colors => ""
+      :players => {
+        :player_name => "",
+
+      }
+    },
+    :away=>{
+      :team_name => ""
+      :colors => ""
+      :players => ""
+    }
+  }
 end
