c820ce7128579672b770f795b11cdf916f8ab71d
Raynaldo Sutisna <raaynaldo@hotmail.com>
2020-09-18 20:47:32 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,12 +6,19 @@ def game_hash
     :home=>{
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => {
-        :player_name => "",
-        :number => "",
-        :shoe=> "",
-        :points=>
-      }
+      :players => [
+        {
+        :player_name => "Alan Anderson",
+        :number => 0,
+        :shoe=> 16,
+        :points=> 22,
+        :rebounds=> 12,
+        :assists=> 12,
+        :steals=>3,
+        :blocks=> 1,
+        :slam_dunks=> 1
+        }
+      ]
     },
     :away=>{
       :team_name => "",
